95052fe9ffe175c00a48c54fcb989cb8390c36e7
codemodeco <codemode.co@gmail.com>
2017-08-27 01:27:29 +0530

    added image

--- LimitlessBackground/canvas.js
+++ LimitlessBackground/canvas.js
@@ -1,1 +1,11 @@
-alert("Ok this is workign!")
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+window.addEventListener('resize', function(){
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+})
+
