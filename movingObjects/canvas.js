8a1646d705da119a4ee2e5db769900a3a3ed814d
codemodeco <codemode.co@gmail.com>
2017-08-27 02:11:41 +0530

    added bock

--- movingObjects/canvas.js
+++ movingObjects/canvas.js
@@ -9,8 +9,6 @@ window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
 })
 
-
-
 function Square(_x,_y,_color){
     this.x = _x;
     this.y = _y;
@@ -20,11 +18,41 @@ function Square(_x,_y,_color){
         context.fillStyle = this.color;
         context.fillRect(_x,_y, 30, 30); //Hardcoding values for height and width for demo purpose;
     }
+    this.update = function(_xOrY,_isPositive){
+        if(true){
+            //if it is x
+            if(_isPositive){
+                
+            }
+        }
+    }
 }
 let img = new Image();
 img.src = 'back.png';
 
 var _sqOne = new Square(300, 300, 'red');
+
+window.addEventListener('keydown', function(event){
+    var char = event.which;
+    if(char == 40){
+        //Key Down
+        console.log("keydown")
+        ++_sqOne.y;
+    }else if(char == 38){
+        //Key Up
+        console.log("key up")
+        --_sqOne.y;
+    }else if(char == 37){
+        //Key Left
+        console.log("key left")
+        --_sqOne.x;
+    }else if(char == 39){
+        //Key Right
+        console.log("right")
+        ++_sqOne.x;
+    }
+    
+})
 
 let _x = 0;
 img.height = canvas.height;
@@ -34,7 +62,6 @@ function animate() {
     if(_x <= -1279){
         _x = 0;
     }
-    console.log(_x)
     context.drawImage(img, _x, 0);
     _sqOne.draw()
     _x--;
