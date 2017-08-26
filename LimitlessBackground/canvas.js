0b2dca5426c056ff33c4d439cb5bded8984bc2f6
codemodeco <codemode.co@gmail.com>
2017-08-27 01:38:42 +0530

    finished basic animating

--- LimitlessBackground/canvas.js
+++ LimitlessBackground/canvas.js
@@ -1,6 +1,6 @@
 let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
-
+//image widht = 1279;
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
@@ -9,3 +9,20 @@ window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
 })
 
+var BackgroundImage = new Image();
+BackgroundImage.src = 'back.png';
+BackgroundImage.height = canvas.height;
+let _x = 0;
+function animate(){
+    context.save();
+    requestAnimationFrame(animate);
+    if(_x <= -1279){
+        _x = 0;
+    }
+    context.drawImage(BackgroundImage, _x, 0);
+    _x--;
+    context.restore();
+    console.log(_x)
+    
+}
+animate();
