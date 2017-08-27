f698d18e06390a250826ca2c18c4e6d1b241be1e
codemodeco <codemode.co@gmail.com>
2017-08-27 23:44:15 +0530

    final commit

--- ShootinGame/canvas.js
+++ ShootinGame/canvas.js
@@ -13,7 +13,6 @@ let windowInfo = {
     x: 0, y: 0
 }
 window.addEventListener('mousemove', function(event){
-    console.log(event.clientX, event.clientY)
         checkAndDeleteSquare(event.clientX, event.clientY);
         windowInfo.x = event.clientX;
         windowInfo.y = event.clientY;
@@ -33,6 +32,7 @@ function Enemies(_health, _height, _width, _x, _y){
     this.y = _y;
 
     this.draw = function(){
+        context.fillStyle = 'cyan'        
         context.fillRect(this.x, this.y, this.width, this.height);
     }
     this.update = function(_x, _y){
@@ -49,35 +49,62 @@ function Enemies(_health, _height, _width, _x, _y){
 var arr = [];
 var y_pos = -60;
 
-var xAxisArray = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000];
 
-for(var index = 0; index < 20; index++){
-    arr.push(new Enemies(100,40, 40, xAxisArray[Math.floor(Math.random() * xAxisArray.length)], y_pos));
-    y_pos -=60;
+function init(){
+
+    for(var index = 0; index < 1000; index++){
+        arr.push(new Enemies(100,40, 40, Math.random() * canvas.width, y_pos));
+        y_pos -=60;
+    }
 }
+init();
+let score = 0;
 
 function checkAndDeleteSquare(_x, _y){
     for(var index = 0; index < arr.length; index++){
         if(_x >= arr[index].x && _x <= (arr[index].width + arr[index].x) &&
             _y >= arr[index].y && _y <= (arr[index].width + arr[index].y)){
-                arr.splice(index, 1);
+                alert('Game over buddy, your score is '+ score);
+                context.clearRect(0,0, canvas.width, canvas.height);
+                arr = [];
+                y_pos = -60;                
+                init();
             }
+        if(arr[index].y > canvas.height){
+            score += 50;
+            arr.splice(index, 1);
+        }
     }
 }
+
+var BackgroundImage = new Image();
+BackgroundImage.src = 'back.png';
+BackgroundImage.height = canvas.height;
+let _x = 0;
 
 function animate(){
     requestAnimationFrame(animate);
     context.clearRect(0,0,canvas.width, canvas.height);
+    if(_x <= -1279){
+        _x = 0;
+    }
+    context.drawImage(BackgroundImage, _x, 0);
+    _x--;
     context.beginPath();
-    context.arc(windowInfo.x, windowInfo.y, 20, 0, Math.PI * 2);
+    context.fillStyle = 'rgba(255,0,0,1)';
+    context.arc(windowInfo.x, windowInfo.y, 5, 0, Math.PI * 2);
     context.fill();
+    context.closePath();
+    // context.font = "20px sans-serif";
+    // context.fillText("Score - "+ score,canvas.width - 100,50);
     var boxX, boxY;
     for(let index = 0; index < arr.length; index ++){
         boxX = arr[index].x;
         boxY = arr[index].y;
         arr[index].update(boxX, boxY);
-        arr[index].y += 1;
+        arr[index].y += 4;
     }
+    console.log(y_pos)
 }
 
 animate();
