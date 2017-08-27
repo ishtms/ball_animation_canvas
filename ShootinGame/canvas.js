f00d286db38534d909970c88ab8e9d71b0fa3b39
codemodeco <codemode.co@gmail.com>
2017-08-27 14:01:43 +0530

    initialized

--- ShootinGame/canvas.js
+++ ShootinGame/canvas.js
@@ -1,1 +1,25 @@
-alert('this is workign')
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+window.addEventListener('resize', function(){
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+});
+
+function Enemies(_health, _height, _width, _x, _y){
+    this.health = _health;
+    this.height = _height;
+    this.width = _width;
+    this.x = _x;
+    this.y = _y;
+
+    this.draw = function(){
+        context.rect(this.x, this.y, this.width, this.height);
+    }
+    this.update = function(){
+        
+    }
+}
