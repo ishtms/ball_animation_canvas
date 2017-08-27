b37c0fb8d306cbb845378fa3595e5f8047512146
codemodeco <codemode.co@gmail.com>
2017-08-27 20:59:07 +0530

    added basic functionalityg

--- ShootinGame/canvas.js
+++ ShootinGame/canvas.js
@@ -19,7 +19,18 @@ function Enemies(_health, _height, _width, _x, _y){
     this.draw = function(){
         context.rect(this.x, this.y, this.width, this.height);
     }
-    this.update = function(){
-        
+    this.update = function(_x, _y, _health){
+        if(_health <= 0){
+            this.height = 0;
+            this.width = 0;
+        }
+        this.health = _health;
+        this.x = _x;
+        this.y = _y;
+        this.draw();
     }
 }
+
+let Enemy = new Enemies(100,40,40,30,30);
+Enemy.draw()
+
